640888a418c7e3f9e2a402773c20a0aabee27af8
keemjoonsung <kjs990114@naver.com>
2025-04-02 20:25:35 +0900

    [feat] sse + streaming 이용한 버스 경로 조회 controller 구현

--- backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/bus/BusController.java
+++ backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/bus/BusController.java
@@ -1,31 +1,32 @@
 package com.ddbb.dingdong.presentation.endpoint.bus;
 
 import com.ddbb.dingdong.application.exception.APIException;
-import com.ddbb.dingdong.application.usecase.bus.GetAvailableBusLine;
-import com.ddbb.dingdong.application.usecase.bus.GetBusSchedulesUseCase;
-import com.ddbb.dingdong.application.usecase.bus.GetBusStopLocationUseCase;
-import com.ddbb.dingdong.application.usecase.bus.GetPathPointLine;
+import com.ddbb.dingdong.application.usecase.bus.*;
 import com.ddbb.dingdong.domain.common.exception.DomainException;
 import com.ddbb.dingdong.domain.reservation.entity.vo.Direction;
 import com.ddbb.dingdong.infrastructure.auth.security.AuthUser;
 import com.ddbb.dingdong.infrastructure.auth.security.annotation.LoginUser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/bus")
 @RequiredArgsConstructor
+@Slf4j
 public class BusController {
     private final GetBusSchedulesUseCase getBusSchedulesUseCase;
     private final GetAvailableBusLine getAvailableBusLine;
     private final GetPathPointLine getPathPointLine;
     private final GetBusStopLocationUseCase getBusStopLocationUseCase;
-
+    private final GetPathPointBySSEUseCase getPathPointBySSEUseCase;
     @GetMapping("/schedule/time")
     public ResponseEntity<GetBusSchedulesUseCase.Response> getBusSchedules(
             @RequestParam("direction") Direction direction,
@@ -66,6 +67,17 @@ public class BusController {
         } catch (DomainException e) {
             throw new APIException(e, HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/v2/path/{busScheduleId}")
+    public ResponseBodyEmitter getPathPointLine2(
+            @LoginUser AuthUser authUser,
+            @PathVariable("busScheduleId") Long busScheduleId
+    ) {
+        ResponseBodyEmitter emitter = new SseEmitter(60 * 1000L); // timeout 1분
+        GetPathPointBySSEUseCase.Param param = new GetPathPointBySSEUseCase.Param(busScheduleId, emitter);
+        getPathPointBySSEUseCase.execute(param);
+        return emitter;
     }
 
     @GetMapping("/bus-stop/location/{busScheduleId}")
